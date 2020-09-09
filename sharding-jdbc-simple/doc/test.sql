f431a115ff8e3825fa655afce348e418d0742e40
Java秀 <javaxiu@javaxiu.com>
2020-09-09 15:07:10 +0800

    develop-提交demo需要的sql语句.

--- sharding-jdbc-simple/doc/test.sql
+++ sharding-jdbc-simple/doc/test.sql
@@ -1,3 +1,4 @@
+-- 水平分表
 CREATE DATABASE `order_db` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 
 DROP TABLE IF EXISTS `t_order_1`;
@@ -16,4 +17,68 @@ CREATE TABLE `t_order_2` (
 `user_id` bigint(20) NOT NULL COMMENT '下单用户id',
 `status` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单状态',
 PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- 水平分库
+-- 库1
+CREATE DATABASE `order_db_1` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+
+DROP TABLE IF EXISTS `t_order_1`;
+CREATE TABLE `t_order_1` (
+`order_id` bigint(20) NOT NULL COMMENT '订单id',
+`price` decimal(10, 2) NOT NULL COMMENT '订单价格',
+`user_id` bigint(20) NOT NULL COMMENT '下单用户id',
+`status` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单状态',
+PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `t_order_2`;
+CREATE TABLE `t_order_2` (
+`order_id` bigint(20) NOT NULL COMMENT '订单id',
+`price` decimal(10, 2) NOT NULL COMMENT '订单价格',
+`user_id` bigint(20) NOT NULL COMMENT '下单用户id',
+`status` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单状态',
+PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- 库2
+CREATE DATABASE `order_db_2` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+
+DROP TABLE IF EXISTS `t_order_1`;
+CREATE TABLE `t_order_1` (
+`order_id` bigint(20) NOT NULL COMMENT '订单id',
+`price` decimal(10, 2) NOT NULL COMMENT '订单价格',
+`user_id` bigint(20) NOT NULL COMMENT '下单用户id',
+`status` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单状态',
+PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+DROP TABLE IF EXISTS `t_order_2`;
+CREATE TABLE `t_order_2` (
+`order_id` bigint(20) NOT NULL COMMENT '订单id',
+`price` decimal(10, 2) NOT NULL COMMENT '订单价格',
+`user_id` bigint(20) NOT NULL COMMENT '下单用户id',
+`status` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '订单状态',
+PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- 垂直分库
+CREATE DATABASE `user_db` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+`user_id` bigint(20) NOT NULL COMMENT '用户id',
+`fullname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户姓名',
+`user_type` char(1) DEFAULT NULL COMMENT '用户类型',
+PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- 公共表
+-- 分别在user_db、order_db_1、order_db_2中创建t_dict表
+CREATE TABLE `t_dict` (
+`dict_id` bigint(20) NOT NULL COMMENT '字典id',
+`type` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '字典类型',
+`code` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '字典编码',
+`value` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '字典值',
+PRIMARY KEY (`dict_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
